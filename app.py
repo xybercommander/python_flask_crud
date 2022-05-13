53d104d4b7c3dcc45957b9a3955fd059189b3fc2
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2022-05-13 20:38:43 +0530

    Added db code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # from email.policy import default
-from flask import Flask, render_template, request, url_for, request
+from flask import Flask, render_template, request, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -22,8 +22,18 @@ class Todo(db.Model):
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
+        new_task = Todo(content = task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
     else:        
-        return render_template("index.html")
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks = tasks)
 
 if __name__ == "__main__":
     app.run(debug = True)
